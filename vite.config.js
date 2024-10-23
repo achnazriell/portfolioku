b4cf11fb6d8804e26736ce5ba2fff47cb933f628
nazriel040608 <acmassirau@gmail.com>
2024-10-23 15:03:17 +0700

    w

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0', // atau 'localhost'
-    port: 3000, // Ganti dengan port yang diinginkan
-  },
-});
+  base: '/nazriel/', // Ganti 'repo-name' dengan nama repository GitHub Anda
+  plugins: [react()]
+})
