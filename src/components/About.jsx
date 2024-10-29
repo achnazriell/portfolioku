d69e52a9d5d4ca633041db4a2f0358e18434d80c
nazriel040608 <acmassirau@gmail.com>
2024-10-29 15:01:07 +0700

    s

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -54,7 +54,7 @@ const About = () => {
 
                         {/* Wrap image with a relative positioned container */}
                         <div className="relative">
-                            <img src='me.png' className="w-0 h-0 lg:w-60 lg:h-80 lg:mb-36 lg:mt-[-55px]" />
+                            <img src='me.png' className="w-0 h-0 lg:w-60 lg:h-80 lg:mb-36 lg:mt-[-60px]" />
                         </div>
                     </div>
                 </section>
