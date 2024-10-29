7a32aaf2dd95fb322a9d5095e2c5197969aad3ef
nazriel040608 <acmassirau@gmail.com>
2024-10-29 14:45:57 +0700

    s

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,6 @@ const About = () => {
 
     return (
         <div className="flex">
-            {/* Ensure Sidebar is clickable by giving it a higher z-index and fixed position */}
             {(location.pathname === "/about" || location.pathname === "/skills") && (
                 <div className="z-10 left-0 ">
                     <Sidebar />
@@ -40,18 +39,23 @@ const About = () => {
             )}
             <div className="relative flex-1">
                 <section className="flex-1 lg:p-6 lg:pt-8">
-                    <h1 className="flex ml-14 lg:ml-36 space-x-3 font-bold  ">
+                    <h1 className="flex ml-14 lg:ml-36 space-x-3 font-bold">
                         <span className="text-3xl lg:text-5xl animate-fadeInUp">About</span>
                         <span className="text-3xl lg:text-5xl animate-fadeInUp">Me</span>
                     </h1>
 
+                    <div className="lg:mt-3 mt-3 ml-14 lg:ml-36 flex">
+                        {/* Wrap paragraph */}
+                        <div className="flex-1">
+                            <p ref={paragraphRef} className="font-poppins text-xs mr-4 lg:mr-36 lg:text-xl mb-20 text-gray-600 dark:text-gray-300 animate-fadeInLeft">
+                                My name is Achmad Nazriel Pradita, I'm from Mojokerto and now I'm learning React as a Front-end developer. I'm learning React by self-exploration because it feels fun for me to do this and I also do freelance projects. I'm a 2nd grade student at SMKN 1 Dlanggu majoring in Rekayasa Perangkat Lunak (Software Engineering) who has a lot of interest in development, especially in websites, and I am currently exploring React.
+                            </p>
+                        </div>
 
-                    {/* Flex container to align the paragraph and icon side by side */}
-                    <div className="lg:mt-3 mt-3 ml-14 lg:ml-36 flex items-end ">
-                        <p ref={paragraphRef} className="font-poppins text-xs mr-4  lg:mr-36 lg:text-xl mb-20 text-gray-600 dark:text-gray-300 animate-fadeInLeft">
-                            My name is Achmad Nazriel Pradita, I'm from Mojokerto and now I'm learning React as a Front-end developer. I'm learning React by self-exploration because it feels fun for me to do this and I also do freelance projects. I'm a 2nd grade student at SMKN 1 Dlanggu majoring in Rekayasa Perangkat Lunak (Software Engineering) who has a lot of interest in development, especially in websites, and I am currently exploring React.
-                        </p>
-                            <img src='me.png' className="w-0 h-0 lg:w-96 lg:h-60 lg:mb-24" />
+                        {/* Wrap image with a relative positioned container */}
+                        <div className="relative">
+                            <img src='me.png' className="w-0 h-0 lg:w-96 lg:h-60 lg:mb-24 lg:mt-[-40px]" />
+                        </div>
                     </div>
                 </section>
             </div>
