927d9a0b58b80c774c2d0df95dc5ad28dfa735a4
nazriel040608 <acmassirau@gmail.com>
2024-10-31 13:10:50 +0700

    h

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       fontSize: {
         'xxs': '0.500rem',
         '2xs': '0.600rem', // Ukuran lebih kecil dari text-xs (10px)
-        '1xl': ['1.100rem', '2rem'], 
+        '1xl': '1.100rem', 
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'], // Tambahkan Poppins sebagai font kustom
