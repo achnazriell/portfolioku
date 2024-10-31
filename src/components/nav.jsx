5a5d35f1bc7b59cfd38ff659db9bb789eb8c9441
nazriel040608 <acmassirau@gmail.com>
2024-10-31 14:03:30 +0700

    s

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -67,7 +67,7 @@ const Nav = () => {
 
             {/* Mobile Navigation */}
             <nav className="block sm:hidden">
-                <button onClick={toggleMenu} className="fixed top-5 right-8 flex flex-col justify-center items-center gap-1 backdrop-blur-sm z-50 h-5 w-5 rounded-md">
+                <button onClick={toggleMenu} className="fixed top-5 right-8 flex flex-col justify-center items-center gap-1 backdrop-blur-sm z-50 h-7 w-7 rounded-md">
                     <div className={`w-5 h-1 bg-black dark:bg-white transition-all ${isOpen ? 'rotate-45 translate-y-[12px]' : ''}`}></div>
                     <div className={`w-5 h-1 bg-black dark:bg-white transition-all ${isOpen ? 'opacity-0' : ''}`}></div>
                     <div className={`w-5 h-1 bg-black dark:bg-white transition-all ${isOpen ? '-rotate-45 -translate-y-[1px]' : ''}`}></div>
