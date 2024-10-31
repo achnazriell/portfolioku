b3f9bca5535f8deaca8a577874e23f0c818cdca6
nazriel040608 <acmassirau@gmail.com>
2024-10-31 13:28:32 +0700

    h

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -102,7 +102,7 @@ const Home = () => {
                     My Favorite Project
                 </h2>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-1 lg:gap-10 space-x-0  mx-2 lg:mx-40 lg:mb-60 mb-20">
-                    <div className="flex flex-col items-start">
+                    <div className="flex flex-col md:items-center lg:items-start">
                         {/* Container gambar yang memiliki posisi relative */}
                         <div className="border-2 rounded-md border-gray-500 dark:border-gray-100 relative">
                             <img src="/project1.png" className="h-40 lg:h-48 w-80 lg:w-96 p-3" alt="React" />
@@ -136,7 +136,7 @@ const Home = () => {
                             <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin6">CSS3</div>
                         </div>
                     </div>
-                    <div className="flex flex-col items-start">
+                    <div className="flex flex-col md:items-center lg:items-start">
                     {/* Container gambar yang memiliki posisi relative */}
                     <div className="border-2 rounded-md border-gray-500 dark:border-gray-100 relative">
                         <img src="/project2.png" className="h-40 lg:h-48 w-80 lg:w-96 p-3" alt="React" />
