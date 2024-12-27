3c7425cc3015537ba28190fe07ae5180569fa3ce
nazriel040608 <acmassirau@gmail.com>
2024-12-27 08:26:43 +0700

    a

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -43,45 +43,45 @@ const Projects = () => {
             <br />
             <br />
             <br />
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-1 lg:gap-10 space-x-0  mx-2 lg:mx-40 lg:mb-60 mb-20">
-                    <div className="flex flex-col items-start">
-                        {/* Container gambar yang memiliki posisi relative */}
-                        <div className="border-2 rounded-md border-gray-500 dark:border-gray-100 relative ">
-                            <img src="/project1.png" className="h-40 lg:h-48 w-80 lg:w-96 p-3 animate-zoomin5" alt="React" />
-                        </div>
-
-                        {/* Container flex untuk heading dan ikon GitHub */}
-                        <div className="flex items-center justify-between w-full mt-2">
-                            <h4 className="font-bold text-xl">Web Comics (Pre Mini)</h4>
-
-                            {/* Ikon GitHub di samping heading */}
-                            <a href="https://github.com/achnazriell/comics.git" className="lg:mr-10 mr-2">
-                                <svg style={{ width: 20 }} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <g>
-                                        <path fill="#00BFFF" d="M5.883 18.653c-.3-.2-.558-.455-.86-.816a50.32 50.32 0 0 1-.466-.579c-.463-.575-.755-.84-1.057-.949a1 1 0 0 1 .676-1.883c.752.27 1.261.735 1.947 1.588-.094-.117.34.427.433.539.19.227.33.365.44.438.204.137.587.196 1.15.14.023-.382.094-.753.202-1.095C5.38 15.31 3.7 13.396 3.7 9.64c0-1.24.37-2.356 1.058-3.292-.218-.894-.185-1.975.302-3.192a1 1 0 0 1 .63-.582c.081-.024.127-.035.208-.047.803-.123 1.937.17 3.415 1.096A11.731 11.731 0 0 1 12 3.315c.912 0 1.818.104 2.684.308 1.477-.933 2.613-1.226 3.422-1.096.085.013.157.03.218.05a1 1 0 0 1 .616.58c.487 1.216.52 2.297.302 3.19.691.936 1.058 2.045 1.058 3.293 0 3.757-1.674 5.665-4.642 6.392.125.415.19.879.19 1.38a300.492 300.492 0 0 1-.012 2.716 1 1 0 0 1-.019 1.958c-1.139.228-1.983-.532-1.983-1.525l.002-.446.005-.705c.005-.708.007-1.338.007-1.998 0-.697-.183-1.152-.425-1.36-.661-.57-.326-1.655.54-1.752 2.967-.333 4.337-1.482 4.337-4.66 0-.955-.312-1.744-.913-2.404a1 1 0 0 1-.19-1.045c.166-.414.237-.957.096-1.614l-.01.003c-.491.139-1.11.44-1.858.949a1 1 0 0 1-.833.135A9.626 9.626 0 0 0 12 5.315c-.89 0-1.772.119-2.592.35a1 1 0 0 1-.83-.134c-.752-.507-1.374-.807-1.868-.947-.144.653-.073 1.194.092 1.607a1 1 0 0 1-.189 1.045C6.016 7.89 5.7 8.694 5.7 9.64c0 3.172 1.371 4.328 4.322 4.66.865.097 1.201 1.177.544 1.748-.192.168-.429.732-.429 1.364v3.15c0 .986-.835 1.725-1.96 1.528a1 1 0 0 1-.04-1.962v-.99c-.91.061-1.662-.088-2.254-.485z" fill-rule="nonzero" />
-                                    </g>
-                                </svg>
-                            </a>
-                        </div>
-
-                        <p className="mb-2 text-xs font-poppins lg:text-sm text-gray-500 dark:text-gray-400 text-left">
-                            web comic project involves both back-end and front-end development. The back end, built with Laravel, handles comic and chapter management, including efficient creation, updating, and organization of content. The front end focuses on delivering a responsive and user-friendly layout, ensuring readers can easily navigate through comics and chapters
-                        </p>
-
-                        <div className="flex flex-wrap gap-2 mb-4">
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin">Laravel</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin1">PHP</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin2">Tailwind</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin3">Blade</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin4">Javascript</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin5">HTML5</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin6">CSS3</div>
-                        </div>
-                    </div>
-                    <div className="flex flex-col items-start">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-1 lg:gap-10 space-x-0  mx-2 lg:mx-10 lg:mb-60 mb-20">
+                <div className="flex flex-col md:items-center lg:items-start max-w-[400px]">
                     {/* Container gambar yang memiliki posisi relative */}
-                    <div className="border-2 rounded-md border-gray-500 dark:border-gray-100 relative ">
-                        <img src="/project2.png" className="h-40 lg:h-48 w-80 lg:w-96 p-3 animate-zoomin5" alt="React" />
+                    <div className="border-2 rounded-md border-gray-500 dark:border-gray-100 relative">
+                        <img src="/project1.png" className="h-40 lg:h-48 w-80 lg:w-[400px] p-3" alt="React" />
+                    </div>
+
+                    {/* Container flex untuk heading dan ikon GitHub */}
+                    <div className="flex items-center justify-between w-full mt-2">
+                        <h4 className="font-bold text-xl">Web Comics (Pre Mini)</h4>
+
+                        {/* Ikon GitHub di samping heading */}
+                        <a href="https://github.com/achnazriel/comics.git" className="lg:mr-3 mr-2">
+                            <svg style={{ width: 20 }} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <g>
+                                    <path fill="#00BFFF" d="M5.883 18.653c-.3-.2-.558-.455-.86-.816a50.32 50.32 0 0 1-.466-.579c-.463-.575-.755-.84-1.057-.949a1 1 0 0 1 .676-1.883c.752.27 1.261.735 1.947 1.588-.094-.117.34.427.433.539.19.227.33.365.44.438.204.137.587.196 1.15.14.023-.382.094-.753.202-1.095C5.38 15.31 3.7 13.396 3.7 9.64c0-1.24.37-2.356 1.058-3.292-.218-.894-.185-1.975.302-3.192a1 1 0 0 1 .63-.582c.081-.024.127-.035.208-.047.803-.123 1.937.17 3.415 1.096A11.731 11.731 0 0 1 12 3.315c.912 0 1.818.104 2.684.308 1.477-.933 2.613-1.226 3.422-1.096.085.013.157.03.218.05a1 1 0 0 1 .616.58c.487 1.216.52 2.297.302 3.19.691.936 1.058 2.045 1.058 3.293 0 3.757-1.674 5.665-4.642 6.392.125.415.19.879.19 1.38a300.492 300.492 0 0 1-.012 2.716 1 1 0 0 1-.019 1.958c-1.139.228-1.983-.532-1.983-1.525l.002-.446.005-.705c.005-.708.007-1.338.007-1.998 0-.697-.183-1.152-.425-1.36-.661-.57-.326-1.655.54-1.752 2.967-.333 4.337-1.482 4.337-4.66 0-.955-.312-1.744-.913-2.404a1 1 0 0 1-.19-1.045c.166-.414.237-.957.096-1.614l-.01.003c-.491.139-1.11.44-1.858.949a1 1 0 0 1-.833.135A9.626 9.626 0 0 0 12 5.315c-.89 0-1.772.119-2.592.35a1 1 0 0 1-.83-.134c-.752-.507-1.374-.807-1.868-.947-.144.653-.073 1.194.092 1.607a1 1 0 0 1-.189 1.045C6.016 7.89 5.7 8.694 5.7 9.64c0 3.172 1.371 4.328 4.322 4.66.865.097 1.201 1.177.544 1.748-.192.168-.429.732-.429 1.364v3.15c0 .986-.835 1.725-1.96 1.528a1 1 0 0 1-.04-1.962v-.99c-.91.061-1.662-.088-2.254-.485z" fill-rule="nonzero" />
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
+
+                    <p className="mb-2 text-xs font-poppins lg:text-sm text-gray-500 dark:text-gray-400 text-left">
+                        web comic project involves both back-end and front-end development. The back end, built with Laravel, handles comic and chapter management, including efficient creation, updating, and organization of content. The front end focuses on delivering a responsive and user-friendly layout, ensuring readers can easily navigate through comics and chapters
+                    </p>
+
+                    <div className="flex flex-wrap gap-2 mb-4">
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin">Laravel</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin1">PHP</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin2">Tailwind</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin3">Blade</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin4">Javascript</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin5">HTML5</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin6">CSS3</div>
+                    </div>
+                </div>
+                <div className="flex flex-col md:items-center lg:items-start">
+                    {/* Container gambar yang memiliki posisi relative */}
+                    <div className="border-2 rounded-md border-gray-500 dark:border-gray-100 relative">
+                        <img src="/project2.png" className="h-40 lg:h-48 w-80 lg:w-96 p-3" alt="React" />
                     </div>
 
                     {/* Container flex untuk heading dan ikon GitHub */}
@@ -97,21 +97,21 @@ const Projects = () => {
                             </svg>
                         </a>
                     </div>
-                        <p className="mb-2 text-xs font-poppins lg:text-sm text-gray-500 dark:text-gray-400 text-left ">
-                            The restaurant management system is designed to streamline and optimize various aspects of restaurant operations. It includes both back-end and front-end functionalities. On the back end, the system manages reservations, menu items, staff, and orders, allowing for efficient handling of customer preferences, seating arrangements, and kitchen operations.
-                        </p>
-                        <div className="flex flex-wrap gap-2 mb-4">
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin" >Laravel</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin1" >PHP</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin2" >Bootstrap</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin3" >Blade</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin4" >Javascript</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin5" >HTML5</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin6" >CSS3</div>
-                            <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin7" >Admin LTE</div>
-                        </div>
+                    <p className="mb-2 text-xs font-poppins lg:text-sm text-gray-500 dark:text-gray-400 text-left ">
+                        The restaurant management system is designed to streamline and optimize various aspects of restaurant operations. It includes both back-end and front-end functionalities. On the back end, the system manages reservations, menu items, staff, and orders, allowing for efficient handling of customer preferences, seating arrangements, and kitchen operations.
+                    </p>
+                    <div className="flex flex-wrap gap-2 mb-4">
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin" >Laravel</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin1" >PHP</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin2" >Bootstrap</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin3" >Blade</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin4" >Javascript</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin5" >HTML5</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin6" >CSS3</div>
+                        <div className="bg-gray-400 lg:text-sm text-xs px-2 py-1 rounded-lg animate-zoomin7" >Admin LTE</div>
                     </div>
                 </div>
+            </div>
         </section>
     );
 }
