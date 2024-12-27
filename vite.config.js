8d0bf585ef48cb03475ece7a4fbbf51b0c0dfb16
nazriel040608 <acmassirau@gmail.com>
2024-12-27 07:49:34 +0700

    g

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // Ganti 'repo-name' dengan nama repository GitHub Anda
+  base: './', 
   plugins: [react()]
 })
