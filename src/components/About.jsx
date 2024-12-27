eb6e008adecdfbb910d9a63b9b592ff3ed8bebe2
nazriel040608 <acmassirau@gmail.com>
2024-12-27 08:45:05 +0700

    tyyyyyyy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -40,14 +40,14 @@ const About = () => {
             <div className="relative flex-1">
                 <section className="flex-1 lg:p-6 lg:pt-8">
                     <h1 className="flex ml-14 lg:ml-36 space-x-3 font-bold">
-                        <span className="text-4xl lg:text-5xl animate-fadeInUp">About</span>
-                        <span className="text-4xl lg:text-5xl animate-fadeInUp">Me</span>
+                        <span className="text-3xl lg:text-6xl animate-fadeInUp">About</span>
+                        <span className="text-3xl lg:text-6xl animate-fadeInUp">Me</span>
                     </h1>
 
                     <div className="lg:mt-3 mt-3 ml-14 lg:ml-36 flex">
                         {/* Wrap paragraph */}
                         <div className="flex-1">
-                            <p ref={paragraphRef} className="font-poppins text-sm mr-4 lg:mr-52 lg:text-xl mb-20 text-gray-600 dark:text-gray-300 animate-fadeInLeft">
+                            <p ref={paragraphRef} className="font-poppins text-xs mr-4 lg:mr-52 lg:text-2xl mb-20 text-gray-600 dark:text-gray-300 animate-fadeInLeft">
                                 My name is Achmad Nazriel Pradita, I'm from Mojokerto and now I'm learning React as a Front-end developer. I'm learning React by self-exploration because it feels fun for me to do this and I also do freelance projects. I'm a 2nd grade student at SMKN 1 Dlanggu majoring in Rekayasa Perangkat Lunak (Software Engineering) who has a lot of interest in development, especially in websites, and I am currently exploring React.
                             </p>
                         </div>
