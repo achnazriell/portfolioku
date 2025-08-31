0a65cb52cdbc28d9902b6bfa27ccb9046e36118b
nazriel040608 <acmassirau@gmail.com>
2025-08-31 20:44:48 +0700

    master

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useEffect, useRef } from "react"
 import { useLocation } from "react-router-dom"
 import Sidebar from "./sidebar"
@@ -56,39 +54,42 @@ const About = () => {
                 </div>
             )}
             <div className="relative flex-1">
-                <section className="flex-col lg:flex-row lg:items-start lg:gap-8 lg:p-6 lg:pt-8">
+                <section className="flex flex-col lg:flex-row lg:items-start lg:gap-12 lg:p-6 lg:pt-8">
+                    {/* Heading */}
                     <h1 className="flex ml-14 lg:ml-36 space-x-3 font-bold">
                         <span className="text-3xl lg:text-6xl animate-fadeInUp">About</span>
                         <span className="text-3xl lg:text-6xl animate-fadeInUp">Me</span>
                     </h1>
 
-                    <div className="lg:mt-3 mt-3 ml-14 lg:ml-36">
-                        <div className="flex-1">
-                            <div className="flex-1 lg:max-w-2xl">
-                                <p
-                                    ref={paragraphRef}
-                                    className="font-poppins text-xs lg:text-xl mb-8 lg:mb-20 text-gray-600 dark:text-gray-300 animate-fadeInLeft leading-relaxed"
-                                >
-                                    My name is Achmad Nazriel Pradita, I'm from Mojokerto and now I'm learning React as a Front-end
-                                    developer. I'm learning React by self-exploration because it feels fun for me to do this and I also do
-                                    freelance projects. I'm a 2nd grade student at SMKN 1 Dlanggu majoring in Rekayasa Perangkat Lunak
-                                    (Software Engineering) who has a lot of interest in development, especially in websites, and I am
-                                    currently exploring React.
-                                </p>
-                            </div>
+                    {/* Kontainer paragraf + gambar */}
+                    <div className="flex flex-col lg:flex-row lg:items-center lg:ml-36 lg:mt-6 gap-10">
+                        {/* Paragraf */}
+                        <div className="flex-1 lg:max-w-2xl">
+                            <p
+                                ref={paragraphRef}
+                                className="font-poppins text-xs lg:text-xl mb-8 text-gray-600 dark:text-gray-300 animate-fadeInLeft leading-relaxed"
+                            >
+                                My name is Achmad Nazriel Pradita, I'm from Mojokerto and now I'm learning React as a Front-end
+                                developer. I'm learning React by self-exploration because it feels fun for me to do this and I also do
+                                freelance projects. I'm a 2nd grade student at SMKN 1 Dlanggu majoring in Rekayasa Perangkat Lunak
+                                (Software Engineering) who has a lot of interest in development, especially in websites, and I am
+                                currently exploring React.
+                            </p>
                         </div>
-                    </div>
-                    <div className="hidden lg:block lg:flex-shrink-0">
-                        <div className="relative">
-                            <img
-                                ref={imageRef}
-                                src="/ngerii.jpg"
-                                alt="Achmad Nazriel Pradita"
-                                className="w-64 h-80 rounded-xl transition-all duration-300 ease-out "
-                                onMouseMove={handleMouseMove}
-                                onMouseLeave={handleMouseLeave}
-                                style={{ transformStyle: "preserve-3d" }}
-                            />
+
+                        {/* Gambar */}
+                        <div className="lg:flex-shrink-0">
+                            <div className="relative">
+                                <img
+                                    ref={imageRef}
+                                    src="/ngerii.jpg"
+                                    alt="Achmad Nazriel Pradita"
+                                    className="w-64 h-80 rounded-xl transition-all duration-300 ease-out"
+                                    onMouseMove={handleMouseMove}
+                                    onMouseLeave={handleMouseLeave}
+                                    style={{ transformStyle: "preserve-3d" }}
+                                />
+                            </div>
                         </div>
                     </div>
                 </section>
